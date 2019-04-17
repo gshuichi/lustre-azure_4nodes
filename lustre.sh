0f7eb0a291484a34bee1a2abd2d6e3399ad17409
gshuichi <shuichi.gojuki@microsoft.com>
2019-04-17 09:39:28 +0900

    Changed the number of MDS to 4

--- lustre.sh
+++ lustre.sh
@@ -7,13 +7,13 @@ sudo -u $ADMIN_USER sh -c "echo StrictHostKeyChecking no > /home/$ADMIN_USER/.ss
 
 sudo -u $ADMIN_USER ssh-keygen -f /home/$ADMIN_USER/.ssh/id_rsa -t rsa -N ''
 
-for h in az-es-mgs az-es-mds0 az-es-mds1 az-es-oss0 az-es-oss1 az-es-oss2 az-es-oss3; do
+for h in az-es-mgs az-es-mds0 az-es-mds1 az-es-mds2 az-es-mds3 az-es-oss0 az-es-oss1 az-es-oss2 az-es-oss3; do
 	sudo -u $ADMIN_USER sh -c "sshpass -p $ADMIN_PASS ssh-copy-id ${h}"
 done
 
-sudo -u $ADMIN_USER clush -w az-es-mgs,az-es-mds[0-1],az-es-oss[0-s31] "sshpass -p $ADMIN_PASS sudo sh -c \"echo $ADMIN_PASS | passwd --stdin root\""
+sudo -u $ADMIN_USER clush -w az-es-mgs,az-es-mds[0-3],az-es-oss[0-3] "sshpass -p $ADMIN_PASS sudo sh -c \"echo $ADMIN_PASS | passwd --stdin root\""
 
-for h in az-es-mgs az-es-mds0 az-es-mds1 az-es-oss0 az-es-oss1 az-es-oss2 az-es-oss3; do
+for h in az-es-mgs az-es-mds0 az-es-mds1 az-es-oss0 az-es-mds2 az-es-mds3 az-es-oss1 az-es-oss2 az-es-oss3; do
 	sudo -u $ADMIN_USER sh -c "sshpass -p $ADMIN_PASS ssh-copy-id root@${h}"
 done
 
@@ -21,7 +21,7 @@ sudo -u $ADMIN_USER ssh root@az-es-mgs mkfs.lustre --mgs --fsname=lustre /dev/sd
 sudo -u $ADMIN_USER ssh root@az-es-mgs mkdir -p /mnt/lustre/mgs
 sudo -u $ADMIN_USER ssh root@az-es-mgs mount -t lustre /dev/sdc /mnt/lustre/mgs
 
-for i in `seq 0 1`; do
+for i in `seq 0 3`; do
 	sudo -u $ADMIN_USER ssh root@az-es-mds${i} mkfs.lustre --mdt --mgsnode=az-es-mgs --index=${i} --fsname=lustre /dev/sdc
 	sudo -u $ADMIN_USER ssh root@az-es-mds${i} mkdir -p /mnt/lustre/mdt${i}
 	sudo -u $ADMIN_USER ssh root@az-es-mds${i} mount -t lustre /dev/sdc /mnt/lustre/mdt${i}
